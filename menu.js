b7b21a4b47b36635062f28477dfd8638579f7cae
Yue Jiao <cleamoon@hotmail.com>
2021-10-22 20:18:13 +0200

    Add save function

--- menu.js
+++ menu.js
@@ -1,4 +1,11 @@
-const { app, Menu, shell } = require("electron")
+const { 
+    app, 
+    Menu, 
+    shell, 
+    BrowserWindow, 
+    globalShortcut, 
+    dialog 
+} = require("electron")
 
 const template = [
     {
@@ -13,20 +20,17 @@ const template = [
         ]
     },
     {
-        label: "Debugging",
+        label: "Format",
         submenu: [
             {
-                lable: "Dev tools",
-                role: "toggleDevTools"
-            },
-
-            { 
-                type: "separator" 
-            },
-
-            { 
-                role: "reload",
-                accelerator: "Alt+R"
+                label: "Toggle Bold",
+                click() {
+                    const window = BrowserWindow.getFocusedWindow();
+                    window.webContents.send(
+                        'editor-event',
+                        'toggle-bold'
+                    );
+                }
             }
         ]
     }
@@ -43,6 +47,65 @@ if (process.platform === "darwin") {
     })
 }
 
+if (process.env.DEBUG) {
+    template.push({
+        label: "Debugging",
+        submenu: [
+            {
+                lable: "Dev tools",
+                role: "toggleDevTools"
+            },
+
+            { 
+                type: "separator" 
+            },
+
+            { 
+                role: "reload",
+                accelerator: "Alt+R"
+            }
+        ]
+    });
+}
+
 const menu = Menu.buildFromTemplate(template);
 
 module.exports = menu; 
+
+const { ipcMain } = require("electron");
+
+ipcMain.on("editor-reply", (event, arg) => {
+    console.log(`Received reply from web page: ${arg}`);
+});
+
+ipcMain.on("save", (event, arg) => {
+    console.log(`Saving content of the file`);
+    console.log(arg);
+
+    const window = BrowserWindow.getFocusedWindow();
+    const options = {
+        title: "Save markdown file",
+        filters: [
+            {
+                name: "MyFile",
+                extensions: ["md"]
+            }
+        ]
+    };
+
+    dialog.showSaveDialog(window, options, filename => {
+        const fs = require('fs');
+        if (filename) {
+            console.log(`Saving content to the file: ${filename}`);
+            fs.writeFileSync(filename, arg);
+        }
+    });
+});
+
+app.on("ready", () => {
+    globalShortcut.register("CommandOrontrol+S", () => {
+        console.log("Saving the file");
+        const window = BrowserWindow.getFocusedWindow();
+        window.webContents.send("editor-event", "save");
+    });
+});
